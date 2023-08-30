12d9fbe179ef77addcbbf5c3fe46d96ae5d5c574
zpc <zip_city@163.com>
2023-08-30 15:42:43 +0800

    Only serve static

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -107,29 +107,28 @@ void read_requesthdrs(rio_t *rp)
  *             return 0 if dynamic content, 1 if static
  */
 /* $begin parse_uri */
-int parse_uri(char *uri, char *filename, char *cgiargs) 
-{
-    char *ptr;
 
-    if (!strstr(uri, "cgi-bin")) {  /* Static content */ //line:netp:parseuri:isstatic
-	strcpy(cgiargs, "");                             //line:netp:parseuri:clearcgi
-	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert1
-	strcat(filename, uri);                           //line:netp:parseuri:endconvert1
-	if (uri[strlen(uri)-1] == '/')                   //line:netp:parseuri:slashcheck
-	    strcat(filename, "home.html");               //line:netp:parseuri:appenddefault
-	return 1;
-    }
-    else {  /* Dynamic content */                        //line:netp:parseuri:isdynamic
-	ptr = index(uri, '?');                           //line:netp:parseuri:beginextract
-	if (ptr) {
-	    strcpy(cgiargs, ptr+1);
-	    *ptr = '\0';
-	}
-	else 
-	    strcpy(cgiargs, "");                         //line:netp:parseuri:endextract
-	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert2
-	strcat(filename, uri);                           //line:netp:parseuri:endconvert2
-	return 0;
+int parse_uri(char *uri, char *filename, char *querys) {
+    char *query_start = strchr(uri, '?');
+
+    // 没有问号，则整个字符串为路径，查询部分为空
+    if (query_start == NULL) {
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        strcpy(querys, "");
+        if(uri[strlen(uri)-1] == '/') {
+            strcat(filename, "home.html");
+        }
+        // printf("==================%s===========================", filename);
+        return 1;
+    } else {
+        // 复制路径部分,最多复制偏移量个字符
+        strncpy(filename, uri, query_start - uri);
+        filename[query_start - uri] = '\0';
+
+        // 复制查询部分,从字符串指定位置开始复制
+        strcpy(querys, query_start + 1);
+        return 0;
     }
 }
 /* $end parse_uri */
